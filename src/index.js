43dec11494384878b250bf58e1f012f0ff9f6dac
Andrew Wang <Andrewruujin@gmail.com>
2022-09-02 19:01:19 -0400

    commit tree f36cd163c5b96f8b8d0659906a7cc562c7bb503d

--- src/index.js
+++ src/index.js
@@ -12,4 +12,65 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+getAllToys()
+document.querySelector('.add-toy-form').addEventListener('submit',handleSubmit)
 });
+
+function renderToy(toy){
+  const div = document.createElement('div')
+  const toyContainer = document.querySelector('#toy-collection')
+  div.className = "card"
+
+  div.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src="${toy.image}" class="toy-avatar" />
+  <p>${toy.likes} Likes</p>
+  <button class="like-btn" id="${toy.id}" onClick="updateLikes(${toy.id})">Like ❤️</button>
+  `
+  div.querySelector('.like-btn').addEventListener('click', () => {
+    toy.likes+= 1
+    div.querySelector('p').textContent = `${toy.likes} Likes`
+    updateLikes(toy)
+  })
+
+
+  toyContainer.appendChild(div)
+}
+
+function updateLikes(toyObj){
+  fetch(`http://localhost:3000/toys/${toyObj.id}`,{
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body:JSON.stringify(toyObj)
+  })
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  let toyObj = {
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes: 0
+  }
+  renderToy(toyObj)
+  addNewToy(toyObj)
+}
+function addNewToy(toyObj){
+  fetch('http://localhost:3000/toys',{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body:JSON.stringify(toyObj)
+  })
+}
+
+function getAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toyData => toyData.forEach(toy => renderToy(toy)))
+}
